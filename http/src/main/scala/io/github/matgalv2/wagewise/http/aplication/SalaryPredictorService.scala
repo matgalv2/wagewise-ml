cd3579766a8e315e143aee891773f31c7e48d6af
Mateusz <matgalv2@gmail.com>
2023-11-12 18:54:54 +0100

    Updated

--- http/src/main/scala/io/github/matgalv2/wagewise/http/aplication/SalaryPredictorService.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/aplication/SalaryPredictorService.scala
@@ -1,19 +1,20 @@
 package io.github.matgalv2.wagewise.http.aplication
 
-import io.github.matgalv2.wagewise.ml.Prediction.EarningsPrediction
-import io.github.matgalv2.wagewise.ml.PredictorError.WrongValues
-import io.github.matgalv2.wagewise.ml.Processing.ProgrammerFeatures
-import io.github.matgalv2.wagewise.ml.{PredictorError, RandomForestRegression}
-import zio.{IO, ZIO}
+
+import io.github.matgalv2.wagewise.ml.RandomForestRegression
+import io.github.matgalv2.wagewise.ml.model.Prediction.EarningsPrediction
+import io.github.matgalv2.wagewise.ml.predictor.PredictorError.WrongValues
+import io.github.matgalv2.wagewise.ml.predictor.{PredictorError, SalaryPredictor}
+import zio.{Has, IO, ZIO}
 
 import scala.util.Try
 
 object SalaryPredictorService {
 
-  def predictSalary(programmers: Seq[ProgrammerFeatures]): IO[PredictorError, Seq[EarningsPrediction]] = {
+  def predictSalary(programmers: Seq[SalaryPredictor.ProgrammerFeatures]): IO[PredictorError, Seq[EarningsPrediction]] = {
     val workHoursInMonth = 160
     ZIO
-      .fromTry(Try(RandomForestRegression.makePrediction(programmers)))
+      .fromTry(Try(RandomForestRegression.predict(programmers)))
       .map(_.map(rate_per_hour => EarningsPrediction(rate_per_hour, rate_per_hour * workHoursInMonth)))
       .mapError(_ => WrongValues)
   }
