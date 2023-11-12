bd9c5135d8f88b604be336d465772494e550127f
Mateusz <matgalv2@gmail.com>
2023-11-12 10:54:18 +0100

    Updated ML handler to use only once PredictSalaryService with array input instead of one object at a time

--- http/src/main/scala/io/github/matgalv2/wagewise/http/api/MlApi.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/api/MlApi.scala
@@ -1,19 +1,18 @@
 package io.github.matgalv2.wagewise.http.api
 
-import http.generated.definitions.{ ErrorResponse, PredictedSalaries, Programmer, Programmers }
-import http.generated.ml.{ MlHandler, PredictSalaryResponse }
+import http.generated.definitions.{ErrorResponse, PredictedSalaries, Programmer, Programmers}
+import http.generated.ml.{MlHandler, PredictSalaryResponse}
 import io.github.matgalv2.wagewise.http.aplication.SalaryPredictorService
+import io.github.matgalv2.wagewise.http.converters.prediction.PredictionToApiOps
 import io.github.matgalv2.wagewise.http.converters.programmer.EmploymentModelOps
 import io.github.matgalv2.wagewise.http.repository.repository.Repository
-import zio.{ Has, RIO, UIO, ZIO }
+import zio.{Has, RIO, UIO}
 
 class MlApi extends MlHandler[RIO[Repository, *]] {
   override def predictSalary(respond: PredictSalaryResponse.type)(body: Programmers): UIO[PredictSalaryResponse] =
-    ZIO
-      .foreach(body.value)(programmer =>
-        SalaryPredictorService.predictSalary(programmer.id, programmer.toModelFeatures)
-      )
-      .map(predictedSalaries => respond.Ok(PredictedSalaries(predictedSalaries)))
+    SalaryPredictorService
+      .predictSalary(body.value.map(_.toModelFeatures))
+      .map(predictedSalaries => respond.Ok(PredictedSalaries(predictedSalaries.zip(body.value).map(earnings_ids => earnings_ids._1.toApi(earnings_ids._2.id)).toVector)))
       .mapError(_ => respond.BadRequest(ErrorResponse(Vector(body.value.mkString("Programmer(", ", ", ")")))))
       .merge
 }
