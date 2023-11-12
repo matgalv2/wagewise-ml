ec9a15e2b087a4ed502ab21fbd9c851b5f5f83e4
Mateusz <matgalv2@gmail.com>
2023-11-12 18:55:43 +0100

    Moved files

--- modules/ml/src/main/scala/io.github.matgalv2.wagewise.ml/Prediction.scala
+++
@@ -1,8 +0,0 @@
-package io.github.matgalv2.wagewise.ml
-
-
-sealed trait Prediction
-
-object Prediction {
-  final case class EarningsPrediction(ratePerHour: Double, salary: Double) extends Prediction
-}
