480cc17b0379a6cacef8a247b7e0d0f392ff3a29
Mateusz <matgalv2@gmail.com>
2023-11-12 18:50:19 +0100

    Updated converter

--- http/src/main/scala/io/github/matgalv2/wagewise/http/api/MlApi.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/api/MlApi.scala
@@ -4,14 +4,14 @@ import http.generated.definitions.{ErrorResponse, PredictedSalaries, Programmer,
 import http.generated.ml.{MlHandler, PredictSalaryResponse}
 import io.github.matgalv2.wagewise.http.aplication.SalaryPredictorService
 import io.github.matgalv2.wagewise.http.converters.prediction.PredictionToApiOps
-import io.github.matgalv2.wagewise.http.converters.programmer.EmploymentModelOps
+import io.github.matgalv2.wagewise.http.converters.programmer.ProgrammerModelOps
 import io.github.matgalv2.wagewise.http.repository.repository.Repository
 import zio.{Has, RIO, UIO}
 
 class MlApi extends MlHandler[RIO[Repository, *]] {
   override def predictSalary(respond: PredictSalaryResponse.type)(body: Programmers): UIO[PredictSalaryResponse] =
     SalaryPredictorService
-      .predictSalary(body.value.map(_.toModelFeatures))
+      .predictSalary(body.value.map(_.toModel))
       .map(predictedSalaries => respond.Ok(PredictedSalaries(predictedSalaries.zip(body.value).map(earnings_ids => earnings_ids._1.toApi(earnings_ids._2.id)).toVector)))
       .mapError(_ => respond.BadRequest(ErrorResponse(Vector(body.value.mkString("Programmer(", ", ", ")")))))
       .merge
