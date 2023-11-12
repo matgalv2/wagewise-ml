d2cfc20da400316384a5ccb59e5d54114a799c6c
Mateusz <matgalv2@gmail.com>
2023-11-12 10:48:49 +0100

    Updated repository to pass it to the layers

--- modules/employments/domain/src/main/scala/io/github/matgalv2/employment/repository/EmploymentRepository.scala
+++ modules/employments/domain/src/main/scala/io/github/matgalv2/employment/repository/EmploymentRepository.scala
@@ -1,8 +1,15 @@
 package io.github.matgalv2.employment.repository
 
 import io.github.matgalv2.employment.model.Employment
-import zio.{ IO, UIO }
+import zio.{Task, ZIO}
+import zio._
 
+//@accessible //too get rid off accessors in companion object
 trait EmploymentRepository {
-  def insert(employment: Employment): UIO[Unit]
+  def insert(employment: Employment): Task[Unit]
+}
+object EmploymentRepository {
+  def insert(employment: Employment): ZIO[Has[EmploymentRepository], Throwable, Unit] = {
+    ZIO.serviceWith[EmploymentRepository](_.insert(employment))
+  }
 }
