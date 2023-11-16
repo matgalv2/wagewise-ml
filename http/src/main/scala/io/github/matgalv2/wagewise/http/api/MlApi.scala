ea2e21f204a4d797b22c2880c8dacd167baf34ac
Mateusz <matgalv2@gmail.com>
2023-11-16 22:34:57 +0100

    Updated

--- http/src/main/scala/io/github/matgalv2/wagewise/http/api/MlApi.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/api/MlApi.scala
@@ -1,21 +1,28 @@
 package io.github.matgalv2.wagewise.http.api
 
-import http.generated.definitions.{ErrorResponse, PredictedSalaries, Programmer, Programmers}
+import http.generated.definitions.{ErrorResponse, PredictedSalaries, PredictedSalary, Programmers}
 import http.generated.ml.{MlHandler, PredictSalaryResponse}
+import io.github.matgalv2.wagewise.http.DummyService
 import io.github.matgalv2.wagewise.http.aplication.SalaryPredictorService
 import io.github.matgalv2.wagewise.http.converters.prediction.PredictionToApiOps
 import io.github.matgalv2.wagewise.http.converters.programmer.ProgrammerModelOps
-import io.github.matgalv2.wagewise.http.repository.repository.Repository
-import zio.{Has, RIO, UIO}
+import zio.{Has, RIO, UIO, ZIO}
 
-class MlApi extends MlHandler[RIO[Repository, *]] {
-  override def predictSalary(respond: PredictSalaryResponse.type)(body: Programmers): UIO[PredictSalaryResponse] =
+//noinspection SimplifyMapBothInspection
+class MlApi extends MlHandler[RIO[MlApi.Environment, *]] {
+  override def predictSalary(respond: PredictSalaryResponse.type)(body: Programmers) =
+//    DummyService.log
+//      .as(Option(3))
+//      .someOrFail()
+//      .as(respond.Ok(PredictedSalaries(Vector[PredictedSalary]())))
+//      .orElseFail(respond.BadRequest(ErrorResponse(Vector(body.value.mkString("Programmer(", ", ", ")")))))
+//      .merge
     SalaryPredictorService
       .predictSalary(body.value.map(_.toModel))
       .map(predictedSalaries => respond.Ok(PredictedSalaries(predictedSalaries.zip(body.value).map(earnings_ids => earnings_ids._1.toApi(earnings_ids._2.id)).toVector)))
-      .mapError(_ => respond.BadRequest(ErrorResponse(Vector(body.value.mkString("Programmer(", ", ", ")")))))
+      .orElseFail(respond.BadRequest(ErrorResponse(Vector(body.value.mkString("Programmer(", ", ", ")")))))
       .merge
 }
 object MlApi {
-  type Environment = Has[Nothing]
+  type Environment = Has[DummyService]
 }
