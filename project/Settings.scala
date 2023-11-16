d303fa8b4c5e0cf5939e1479f868377e9deaf6cc
Mateusz <matgalv2@gmail.com>
2023-11-16 22:30:32 +0100

    Fixed exposed ports

--- project/Settings.scala
+++ project/Settings.scala
@@ -7,16 +7,11 @@ object Settings {
 
   val docker =
     List(
-      dockerExposedPorts := Seq(8080, 8080),
+      dockerExposedPorts := Seq(8080),
       dockerBaseImage := "openjdk:11-jdk-slim",
       dockerRepository := Some("wagewise-ml"),
       Docker / packageName := "wagewise-ml",
       dockerUpdateLatest := true,
-//      dockerBuildCommand += "COPY src/main/resources/employments.csv opt/docker/employments.csv",
-//      dockerBuildOptions += "COPY src/main/resources/employments.csv opt/docker/employments.csv",
-//      dockerCommands := dockerCommands.value :+ Cmd("COPY", "modules/ml/src/main/resources/employments.csv", "/opt/docker/employments.csv"),
-//      dockerCommands += Cmd("COPY", "src/main/resources/employments.csv", "/opt/docker/employments.csv"),
-//      dockerExecCommand += "COPY src/main/resources/employments.csv opt/docker/employments.csv",
       dockerEnvVars += "EMPLOYMENTS_DATASET_PATH" -> "/opt/docker/employments.csv"
     )
 }
