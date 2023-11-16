9897cd28e62ee8f7dc4296c19468b72c73df5f04
Mateusz <matgalv2@gmail.com>
2023-11-16 22:31:29 +0100

    Got rid of repository service

--- http/src/main/scala/io/github/matgalv2/wagewise/http/Controller.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/Controller.scala
@@ -1,23 +1,20 @@
 package io.github.matgalv2.wagewise.http
 
-import http.generated.definitions.Programmer
+
 import http.generated.ml.MlResource
 import io.github.matgalv2.wagewise.http.api.MlApi
-import io.github.matgalv2.wagewise.http.repository.repository
-import zio.{Has, RIO, Ref, Runtime, ZEnv, ZIO}
+import zio.{&, Has, RIO, Runtime, ZEnv, ZIO}
 
-import java.time.LocalDate
-import java.util.UUID
 
 object Controller {
 
   /** An effect which, when executed, gives a StoreResource (capable of transforming a StoreHandler into something
     * bindable)
     */
-  val makeMlResource: RIO[repository.Repository, MlResource[RIO[repository.Repository, *]]] = {
+  val makeMlResource: RIO[Has[DummyService], MlResource[RIO[Has[DummyService], *]]] = {
     import zio.interop.catz._
-    ZIO.runtime[repository.Repository].map { implicit r: Runtime[repository.Repository] =>
-      new MlResource[RIO[repository.Repository, *]]
+    ZIO.runtime[Has[DummyService]].map { implicit r: Runtime[Has[DummyService]] =>
+      new MlResource[RIO[Has[DummyService], *]]
     }
   }
 
@@ -25,32 +22,6 @@ object Controller {
     */
   val handler = new MlApi()
 
-  val programmer = Programmer(
-    id                      = UUID.fromString("95fe99be-29c7-4a7c-880f-fb31d92ce3a8"),
-    dateOfEmployment        = LocalDate.of(2000, 3, 29),
-    age                     = 23,
-    sex                     = Programmer.Sex.F,
-    country                 = "Poland",
-    experienceYearsIt       = 0,
-    languages               = "Polish,English",
-    speciality              = Programmer.Speciality.ComputerScientist,
-    coreProgrammingLanguage = Programmer.CoreProgrammingLanguage.Python,
-    academicTitle           = Programmer.AcademicTitle.Bachelor,
-    educationTowardsIt      = false,
-    companyCountry          = "Poland",
-    companyType             = Programmer.CompanyType.SoftwareHouse,
-    workForm                = Programmer.WorkForm.Stationary,
-    teamSize                = 10,
-    teamType                = Programmer.TeamType.Local,
-    formOfEmployment        = Programmer.FormOfEmployment.Employee,
-    fullTime                = true,
-    paidDaysOff             = true,
-    insurance               = true,
-    trainingSessions        = true
-  )
-
-  val initialProgrammers = Vector(programmer)
-  val programmersLayer   = Ref.make(initialProgrammers).toManaged_.toLayer
 
   val combineRoutes = {
     import org.http4s.implicits._
@@ -61,16 +32,15 @@ object Controller {
     } yield mlResource.routes(handler).orNotFound
   }
 
-  val inMemoryLayer = programmersLayer >>> repository.Repository.inMemory
-
-  val prog =
+  val server: ZIO[Has[HttpServer] with Has[DummyService], Throwable, Nothing] =
     for {
       combinedRoutes <- combineRoutes
       binding        <- HttpServer.bindServer(combinedRoutes)
       res            <- binding.use(_ => ZIO.never)
     } yield res
 
-  val inMemoryProg: ZIO[zio.ZEnv with Has[HttpServer], Throwable, Nothing] =
-    prog
-      .provideSomeLayer[ZEnv with Has[HttpServer]](inMemoryLayer)
+//  val inMemoryProg: ZIO[zio.ZEnv & Has[HttpServer], Throwable, Nothing] =
+//    prog
+//      .provideSomeLayer[ZEnv & Has[HttpServer]](DummyImpl.layer)
+
 }
