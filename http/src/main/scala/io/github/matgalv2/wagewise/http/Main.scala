08477c42054a21a0ca2e32ee8bfac5a7526ea466
Mateusz <matgalv2@gmail.com>
2023-11-18 18:01:31 +0100

    Injected Logging and SalaryPredictor

--- http/src/main/scala/io/github/matgalv2/wagewise/http/Main.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/Main.scala
@@ -1,9 +1,9 @@
 package io.github.matgalv2.wagewise.http
 
-
 import io.github.matgalv2.wagewise.ml.RandomForestRegression
+import io.github.matgalv2.wagewise.ml.predictor.SalaryPredictor
+import zio.logging.Logging
 import zio.{&, App, ExitCode, Has, URIO, ZEnv}
-
 
 object Main extends App {
 
@@ -11,11 +11,14 @@ object Main extends App {
     val _ = RandomForestRegression
     print(s"Starting server at ${HttpServer.host}:${HttpServer.port}\n")
     DummyService.log.provideLayer(DummyImpl.layer).exitCode <&
-    Controller
-      .server
-      .provideSomeLayer[ZEnv & Has[HttpServer]](DummyImpl.layer)
-      .exitCode
-      .provideSomeLayer[ZEnv](HttpServer.live)
+      Controller.server
+        .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService] & Has[
+          SalaryPredictor]](Logging.console())
+        .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService]](
+          RandomForestRegression.layer)
+        .provideSomeLayer[ZEnv & Has[HttpServer]](DummyImpl.layer)
+        .exitCode
+        .provideSomeLayer[ZEnv](HttpServer.live)
   }
 
 }
