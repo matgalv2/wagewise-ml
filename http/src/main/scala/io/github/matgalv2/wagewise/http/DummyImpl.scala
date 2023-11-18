33f855b8d77a75225f283585fb3aecf9bb35e765
Mateusz <matgalv2@gmail.com>
2023-11-18 17:59:17 +0100

    Updated

--- http/src/main/scala/io/github/matgalv2/wagewise/http/DummyImpl.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/DummyImpl.scala
@@ -1,15 +1,15 @@
 package io.github.matgalv2.wagewise.http
 
-import zio.blocking.Blocking.Service
-import zio.{Has, UIO, ULayer, ZEnv, ZIO, ZLayer}
+import zio.{Has, UIO, ULayer, ZIO, ZLayer}
 case class DummyImpl() extends DummyService {
   override def log: UIO[Unit] = ZIO.succeed(println("Dummy service is working!"))
 }
 
 object DummyImpl{
-  def layer: ZLayer[Any, Nothing, Has[DummyService]] =
+  val layer: ULayer[Has[DummyService]] =
     ZLayer.succeed(DummyImpl())
 
+//  def layer =
   //    ZLayer.succeed(
 //      new DummyService {
 //        override def log: UIO[Unit] = ZIO.succeed(println("Dummy service is working!"))
