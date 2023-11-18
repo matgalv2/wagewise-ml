85a9b6a37ede42460e753b95be150be60726fb39
Mateusz <matgalv2@gmail.com>
2023-11-18 17:52:13 +0100

    Added errors for ml infrastructure

---
+++ modules/ml/infrastructure/src/main/scala/io.github.matgalv2.wagewise.ml/MlError.scala
@@ -0,0 +1,25 @@
+package io.github.matgalv2.wagewise.ml
+
+import io.github.matgalv2.wagewise.ml.predictor.PredictorError
+
+sealed trait MlError extends PredictorError
+
+object MlError {
+
+  sealed trait SparkError extends MlError
+
+  object SparkError {
+
+    final case class MasterURLCannotBeParsed(url: String) extends SparkError
+    final case class ColumnNotFound(columns: String*) extends SparkError
+    final case class ColumnCannotBeParsedToSpecifiedType(column: String, `type`: String) extends SparkError
+
+    // too broad error - it must be divide into many smaller ones
+    final case object AssemblingDataFailed extends SparkError
+  }
+
+  final case class EnvironmentVariableIsNotSet(env: String) extends MlError
+  final case class DatasetCannotBeFound(path: String) extends MlError
+  final case class SthWrongWithFitting(message: String) extends MlError
+
+}
