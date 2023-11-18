89423799378034286245c86b609efef1f343104a
Mateusz <matgalv2@gmail.com>
2023-11-18 18:03:09 +0100

    Updated service to use injected SalaryPredictor

--- http/src/main/scala/io/github/matgalv2/wagewise/http/aplication/SalaryPredictorService.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/aplication/SalaryPredictorService.scala
@@ -1,21 +1,16 @@
 package io.github.matgalv2.wagewise.http.aplication
 
 
-import io.github.matgalv2.wagewise.ml.RandomForestRegression
 import io.github.matgalv2.wagewise.ml.model.Prediction.EarningsPrediction
-import io.github.matgalv2.wagewise.ml.predictor.PredictorError.WrongValues
 import io.github.matgalv2.wagewise.ml.predictor.{PredictorError, SalaryPredictor}
-import zio.{Has, IO, ZIO}
+import zio.{Has, ZIO}
 
-import scala.util.Try
 
 object SalaryPredictorService {
 
-  def predictSalary(programmers: Seq[SalaryPredictor.ProgrammerFeatures]): IO[PredictorError, Seq[EarningsPrediction]] = {
+  def predictSalary(programmers: Seq[SalaryPredictor.ProgrammerFeatures]): ZIO[Has[SalaryPredictor], PredictorError, Seq[EarningsPrediction]] = {
     val workHoursInMonth = 160
-    ZIO
-      .fromTry(Try(RandomForestRegression.predict(programmers)))
+    SalaryPredictor.predict(programmers)
       .map(_.map(rate_per_hour => EarningsPrediction(rate_per_hour, rate_per_hour * workHoursInMonth)))
-      .mapError(_ => WrongValues)
   }
 }
