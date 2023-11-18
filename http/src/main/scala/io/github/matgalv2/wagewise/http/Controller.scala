2d0e1485fd70700d6bf1094e73debbc0df6a12b3
Mateusz <matgalv2@gmail.com>
2023-11-18 18:04:17 +0100

    Updated controller (logging)

--- http/src/main/scala/io/github/matgalv2/wagewise/http/Controller.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/Controller.scala
@@ -1,29 +1,35 @@
 package io.github.matgalv2.wagewise.http
 
-
 import http.generated.ml.MlResource
 import io.github.matgalv2.wagewise.http.api.MlApi
-import zio.{&, Has, RIO, Runtime, ZEnv, ZIO}
-
+import io.github.matgalv2.wagewise.ml.predictor.SalaryPredictor
+import zio.logging.Logging
+import zio.{&, Has, RIO, Runtime, ZIO}
 
 object Controller {
 
   /** An effect which, when executed, gives a StoreResource (capable of transforming a StoreHandler into something
     * bindable)
     */
-  val makeMlResource: RIO[Has[DummyService], MlResource[RIO[Has[DummyService], *]]] = {
+  val makeMlResource: RIO[
+    Has[SalaryPredictor] with Has[DummyService] with Logging,
+    MlResource[
+      RIO[Has[SalaryPredictor] with Has[DummyService] with Logging, *]]] = {
     import zio.interop.catz._
-    ZIO.runtime[Has[DummyService]].map { implicit r: Runtime[Has[DummyService]] =>
-      new MlResource[RIO[Has[DummyService], *]]
+    ZIO.runtime[Has[SalaryPredictor] with Has[DummyService] with Logging].map {
+      implicit r: Runtime[
+        Has[SalaryPredictor] with Has[DummyService] with Logging] =>
+        new MlResource[
+          RIO[Has[SalaryPredictor] with Has[DummyService] with Logging, *]
+        ]
     }
   }
 
   /** Our HTTP server implementation, utilizing the Repository Layer
     */
-  val handler = new MlApi()
+  private val handler = new MlApi()
 
-
-  val combineRoutes = {
+  private val combineRoutes = {
     import org.http4s.implicits._
     import zio.interop.catz._
 
@@ -32,15 +38,14 @@ object Controller {
     } yield mlResource.routes(handler).orNotFound
   }
 
-  val server: ZIO[Has[HttpServer] with Has[DummyService], Throwable, Nothing] =
+  val server
+    : ZIO[Has[HttpServer] & Has[SalaryPredictor] & Has[DummyService] & Logging,
+          Throwable,
+          Nothing] =
     for {
       combinedRoutes <- combineRoutes
       binding        <- HttpServer.bindServer(combinedRoutes)
       res            <- binding.use(_ => ZIO.never)
     } yield res
-
-//  val inMemoryProg: ZIO[zio.ZEnv & Has[HttpServer], Throwable, Nothing] =
-//    prog
-//      .provideSomeLayer[ZEnv & Has[HttpServer]](DummyImpl.layer)
 
 }
