6de629dfaadfda65e6dd625e2e4a71aeb8929384
Mateusz <matgalv2@gmail.com>
2023-11-18 18:00:20 +0100

    Updated errors

--- modules/ml/domain/src/main/scala/io/github/matgalv2/wagewise/ml/predictor/SalaryPredictor.scala
+++ modules/ml/domain/src/main/scala/io/github/matgalv2/wagewise/ml/predictor/SalaryPredictor.scala
@@ -1,13 +1,13 @@
 package io.github.matgalv2.wagewise.ml.predictor
 
 import io.github.matgalv2.employment.model.Employment
-import zio.{Has, UIO, ZIO}
+import zio.{Has, IO, UIO, ZIO}
 
 trait SalaryPredictor {
-  def predict(programmers: Seq[SalaryPredictor.ProgrammerFeatures]): UIO[Seq[Double]]
+  def predict(programmers: Seq[SalaryPredictor.ProgrammerFeatures]): IO[PredictorError, Seq[Double]]
 }
 object SalaryPredictor{
   type ProgrammerFeatures = Employment
-  def predict(programmers: Seq[SalaryPredictor.ProgrammerFeatures]): ZIO[Has[SalaryPredictor], Throwable, Seq[Double]] =
+  def predict(programmers: Seq[SalaryPredictor.ProgrammerFeatures]): ZIO[Has[SalaryPredictor], PredictorError, Seq[Double]] =
     ZIO.serviceWith[SalaryPredictor](_.predict(programmers))
 }
