702d9e8c02b79f4c0f3e8ddb8528a3b1ef9b7411
Mateusz <matgalv2@gmail.com>
2023-12-09 19:51:29 +0100

    Fixed style

--- http/src/main/scala/io/github/matgalv2/wagewise/http/DummyService.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/DummyService.scala
@@ -1,12 +1,11 @@
 package io.github.matgalv2.wagewise.http
 
-import zio.{Has, UIO, ZIO}
+import zio.{ Has, UIO, ZIO }
 
 trait DummyService {
   def log: UIO[Unit]
 }
-object DummyService{
-  def log: ZIO[Has[DummyService], Nothing, Unit] = {
+object DummyService {
+  def log: ZIO[Has[DummyService], Nothing, Unit] =
     ZIO.serviceWith[DummyService](_.log)
-  }
 }
