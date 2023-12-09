3784338880055237d1f9cdea03b748ace3f5004e
Mateusz <matgalv2@gmail.com>
2023-12-09 19:46:21 +0100

    Added CannotCastData error

--- modules/ml/infrastructure/src/main/scala/io.github.matgalv2.wagewise.ml/MlError.scala
+++ modules/ml/infrastructure/src/main/scala/io.github.matgalv2.wagewise.ml/MlError.scala
@@ -12,7 +12,8 @@ object MlError {
 
     final case class MasterURLCannotBeParsed(url: String) extends SparkError
     final case class ColumnNotFound(columns: String*) extends SparkError
-    final case class ColumnCannotBeParsedToSpecifiedType(column: String, `type`: String) extends SparkError
+    final case class ColumnCannotBeCastToSpecifiedType(column: String, `type`: String) extends SparkError
+    final case class CannotCastData(message: String) extends SparkError
 
     // too broad error - it must be divide into many smaller ones
     final case object AssemblingDataFailed extends SparkError
