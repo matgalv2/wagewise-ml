b0fa32892ebcd33ff7002ad406bcab0136f5a9d1
Mateusz <matgalv2@gmail.com>
2023-12-09 19:46:52 +0100

    Updated

--- http/src/main/scala/io/github/matgalv2/wagewise/http/repository/package.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/repository/package.scala
@@ -1,27 +1,27 @@
-package io.github.matgalv2.wagewise.http.repository
-
-import http.generated.definitions.{ Programmer, Programmers }
-import zio._
-
-package object repository {
-  type Repository = Has[Repository.Service]
-
-  /** Accessor proxies
-    */
-  def getProgrammers = ZIO.accessM[Repository](_.get.getProgrammers)
-
-  object Repository {
-    trait Service {
-      def getProgrammers: UIO[Programmers]
-    }
-
-    /** Simple in-memory implementation
-      */
-    val inMemory = ZLayer.fromService[Ref[Vector[Programmer]], Service](programmers =>
-      new Service {
-        def getProgrammers = programmers.get.map(Programmers.apply)
-
-      }
-    )
-  }
-}
+//package io.github.matgalv2.wagewise.http.repository
+//
+//import http.generated.definitions.{ Programmer, Programmers }
+//import zio._
+//
+//package object repository {
+//  type Repository = Has[Repository.Service]
+//
+//  /** Accessor proxies
+//    */
+//  def getProgrammers = ZIO.accessM[Repository](_.get.getProgrammers)
+//
+//  object Repository {
+//    trait Service {
+//      def getProgrammers: UIO[Programmers]
+//    }
+//
+//    /** Simple in-memory implementation
+//      */
+//    val inMemory = ZLayer.fromService[Ref[Vector[Programmer]], Service](programmers =>
+//      new Service {
+//        def getProgrammers = programmers.get.map(Programmers.apply)
+//
+//      }
+//    )
+//  }
+//}
