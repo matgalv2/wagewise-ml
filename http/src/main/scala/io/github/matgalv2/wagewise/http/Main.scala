ddb0934ed7295352246c35efd0b9047abbbb26c8
Mateusz <matgalv2@gmail.com>
2023-12-09 19:41:47 +0100

    Fixed style

--- http/src/main/scala/io/github/matgalv2/wagewise/http/Main.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/Main.scala
@@ -1,24 +1,17 @@
 package io.github.matgalv2.wagewise.http
 
-import io.github.matgalv2.wagewise.ml.RandomForestRegression
+import io.github.matgalv2.wagewise.logging.{ Logger, Logging }
+import io.github.matgalv2.wagewise.ml.SalaryPredictorRandomForestRegressor
 import io.github.matgalv2.wagewise.ml.predictor.SalaryPredictor
-import zio.logging.Logging
-import zio.{&, App, ExitCode, Has, URIO, ZEnv}
-
+import zio.{ &, App, ExitCode, Has, URIO, ZEnv, ZIO }
 object Main extends App {
 
-  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    val _ = RandomForestRegression
-    print(s"Starting server at ${HttpServer.host}:${HttpServer.port}\n")
-    DummyService.log.provideLayer(DummyImpl.layer).exitCode <&
-      Controller.server
-        .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService] & Has[
-          SalaryPredictor]](Logging.console())
-        .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService]](
-          RandomForestRegression.layer)
-        .provideSomeLayer[ZEnv & Has[HttpServer]](DummyImpl.layer)
-        .exitCode
-        .provideSomeLayer[ZEnv](HttpServer.live)
-  }
+  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    Controller.server
+      .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService] & Has[SalaryPredictor]](Logger.layer)
+      .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService]](SalaryPredictorRandomForestRegressor.layer)
+      .provideSomeLayer[ZEnv & Has[HttpServer]](DummyImpl.layer)
+      .provideSomeLayer[ZEnv](HttpServer.live)
+      .exitCode
 
 }
