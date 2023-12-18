4efaafae3db7a676b3759c88cd56b8dbf6be6fec
Mateusz <matgalv2@gmail.com>
2023-12-18 22:15:20 +0100

    Changed layers composition

--- http/src/main/scala/io/github/matgalv2/wagewise/http/Main.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/Main.scala
@@ -1,17 +1,15 @@
 package io.github.matgalv2.wagewise.http
 
-import io.github.matgalv2.wagewise.logging.{ Logger, Logging }
+import io.github.matgalv2.wagewise.logging.Logger
 import io.github.matgalv2.wagewise.ml.SalaryPredictorRandomForestRegressor
-import io.github.matgalv2.wagewise.ml.predictor.SalaryPredictor
-import zio.{ &, App, ExitCode, Has, URIO, ZEnv, ZIO }
+import zio.{ App, ExitCode, URIO, ZEnv }
 object Main extends App {
 
-  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+  def run(args: List[String]): URIO[ZEnv, ExitCode] =
     Controller.server
-      .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService] & Has[SalaryPredictor]](Logger.layer)
-      .provideSomeLayer[ZEnv & Has[HttpServer] & Has[DummyService]](SalaryPredictorRandomForestRegressor.layer)
-      .provideSomeLayer[ZEnv & Has[HttpServer]](DummyImpl.layer)
-      .provideSomeLayer[ZEnv](HttpServer.live)
+      .provideLayer(dependencies)
       .exitCode
 
+  private val dependencies =
+    ZEnv.live ++ Logger.layer >+> SalaryPredictorRandomForestRegressor.layer ++ DummyImpl.layer ++ HttpServer.live ++ Logger.layer
 }
