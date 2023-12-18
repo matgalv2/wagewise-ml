c7f7f8730336da232500b0265befe082df330cba
Mateusz <matgalv2@gmail.com>
2023-12-18 22:02:50 +0100

    Updated

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,7 +1,5 @@
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport._
 import com.typesafe.sbt.packager.Keys.packageName
-import com.typesafe.sbt.packager.docker.Cmd
-
 
 object Settings {
 
@@ -12,7 +10,13 @@ object Settings {
       dockerRepository := Some("wagewise-ml"),
       Docker / packageName := "wagewise-ml",
       dockerUpdateLatest := true,
+//      dockerBuildCommand += "COPY src/main/resources/employments.csv opt/docker/employments.csv",
+//      dockerBuildOptions += "COPY src/main/resources/employments.csv opt/docker/employments.csv",
+//      dockerCommands := dockerCommands.value :+ Seq(Cmd("COPY", "modules/ml/infrastructure/src/main/resources/employments.csv", "/opt/docker/employments.csv")),
+//      dockerCommands += Cmd("COPY", "modules/ml/infrastructure/src/main/resources/employments.csv", "/opt/docker/employments.csv"),
+//      Universal / mappings += ((sourceDirectory.value / "modules" /"ml"/"infrastructure"/"src"/"main"/"resources"/"employments.csv"), "modules/ml/infrastructure/src/main/resources/employments.csv"),
+//      dockerCommands += Cmd("COPY", "src/main/resources/employments.csv", "/opt/docker/employments.csv"),
+//      dockerExecCommand += "COPY src/main/resources/employments.csv opt/docker/employments.csv",
       dockerEnvVars += "EMPLOYMENTS_DATASET_PATH" -> "/opt/docker/employments.csv"
     )
 }
-
