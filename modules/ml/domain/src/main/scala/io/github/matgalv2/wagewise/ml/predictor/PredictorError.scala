215dbe3ac1c29a482e56bb34fe2088eaa6d87d7d
Mateusz <matgalv2@gmail.com>
2023-12-26 12:46:01 +0100

    Moved content to PredictorError

--- modules/ml/domain/src/main/scala/io/github/matgalv2/wagewise/ml/predictor/PredictorError.scala
+++ modules/ml/domain/src/main/scala/io/github/matgalv2/wagewise/ml/predictor/PredictorError.scala
@@ -1,5 +1,21 @@
 package io.github.matgalv2.wagewise.ml.predictor
 
-trait PredictorError
+sealed trait PredictorError extends Serializable with Product
 
+object PredictorError {
 
+  sealed trait SparkError extends PredictorError
+
+  object SparkError {
+    final case class MasterURLCannotBeParsed(url: String) extends SparkError
+    final case class ColumnNotFound(columns: String*) extends SparkError
+    final case class ColumnCannotBeCastToSpecifiedType(column: String, `type`: String) extends SparkError
+
+    final case object AssemblingDataFailed extends SparkError
+    final case class ModelNotFound(path: String) extends SparkError
+    final case class CannotSaveModel(path: String) extends SparkError
+  }
+
+  final case class NoSuchEnvironmentVariable(env: String) extends PredictorError
+  final case class DatasetCannotBeFound(path: String) extends PredictorError
+}
