2b1871c092f3d40938a4ecf855027ae3cdb46ace
Mateusz <matgalv2@gmail.com>
2024-02-22 16:21:15 +0100

    Updated config

--- http/src/main/scala/io/github/matgalv2/wagewise/http/Config.scala
+++ http/src/main/scala/io/github/matgalv2/wagewise/http/Config.scala
@@ -2,31 +2,41 @@ package io.github.matgalv2.wagewise.http
 
 import zio.config._
 import zio.config.magnolia._
+import com.comcast.ip4s.{ Host, Port }
 
-final case class Config(http: Config.Http, mongo: Config.Mongo, keycloak: Config.Keycloak)
+final case class Config(http: Config.Http, mongo: Config.Mongo, keycloak: Config.Keycloak, spark: Config.Spark)
 
 object Config {
   implicit val descriptor: ConfigDescriptor[Config] =
-    (ConfigDescriptor.nested("http")(httpDescriptor) |@| ConfigDescriptor.nested("mongo")(
+    (ConfigDescriptor.nested("http")(Http.httpDescriptor) |@| ConfigDescriptor.nested("mongo")(
       mongoDescriptor
-    ) |@| ConfigDescriptor.nested("keycloak")(keycloakDescriptor))(Config.apply, Config.unapply)
+    ) |@| ConfigDescriptor.nested("keycloak")(keycloakDescriptor) |@| ConfigDescriptor.nested("spark")(
+      sparkDescriptor
+    ))(Config.apply, Config.unapply)
 
-//  final case class Api(key: String)
-//
-//  object Api {
-//    implicit val apiDescriptor: ConfigDescriptor[Api] = DeriveConfigDescriptor.descriptor
-//  }
+  final case class Http(port: Port, host: Host)
 
-  final case class Http(port: String, host: String)
+  object Http {
+    implicit val hostDescriptor: ConfigDescriptor[Host] =
+      ConfigDescriptor.string.transformOrFailLeft(Host.fromString(_).toRight("No host found"))(_.toString())
 
-  implicit val httpDescriptor: ConfigDescriptor[Http] = DeriveConfigDescriptor.descriptor
+    implicit val portDescriptor: ConfigDescriptor[Port] =
+      ConfigDescriptor.int.transformOrFailLeft(Port.fromInt(_).toRight("No port found"))(_.value)
 
-  final case class Mongo(url: String, database: String)
+    implicit val httpDescriptor: ConfigDescriptor[Http] = (ConfigDescriptor.nested("port")(
+      portDescriptor
+    ) |@| ConfigDescriptor.nested("host")(hostDescriptor))(Http.apply, Http.unapply)
+  }
+
+  final case class Mongo(url: String, database: String, user: String, password: String)
 
   implicit val mongoDescriptor: ConfigDescriptor[Mongo] = DeriveConfigDescriptor.descriptor
 
-  final case class Keycloak(clientSecret: String)
+  final case class Keycloak(url: String, realm: String, clientID: String, clientSecret: String)
 
   implicit val keycloakDescriptor: ConfigDescriptor[Keycloak] = DeriveConfigDescriptor.descriptor
 
+  final case class Spark(masterURL: String, employmentsDatasetPath: String)
+
+  implicit val sparkDescriptor: ConfigDescriptor[Spark] = DeriveConfigDescriptor.descriptor
 }
